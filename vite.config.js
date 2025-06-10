2ca2fe4920dbf7658768ccb3f836b0664ee54030
Anton Nageh <anton.nageh20@gmail.com>
2025-06-10 11:18:58 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
-  ],
-})
+      VitePWA({  
+      registerType: 'prompt',  
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],  
+      manifest: {  
+        name: 'Burger Station',  
+        short_name: 'Burger Station',
+        display: 'standalone',
+        description: 'Made your way. Always fresh.',  
+        theme_color: '#ffffff',  
+        start_url: '/',  
+        icons: [  
+  {  
+  src: 'pwa-192x192.png',  
+            sizes: '192x192',  
+            type: 'image/png',  
+          },  
+          {  
+  src: 'pwa-512x512.png',  
+            sizes: '512x512',  
+            type: 'image/png',  
+          },  
+          {  
+  src: 'pwa-512x512.png',  
+            sizes: '512x512',  
+            type: 'image/png',  
+            purpose: 'any maskable',  
+          },  
+        ],  
+      },  
+    }),  
+  ],  
+});
