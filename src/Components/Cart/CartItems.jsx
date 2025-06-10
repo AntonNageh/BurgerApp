d4f6db2b40d7d6da6eb2a33f1e9051764eae017a
Anton Nageh <anton.nageh20@gmail.com>
2025-06-10 12:36:41 +0300

    onlinepath

--- src/Components/Cart/CartItems.jsx
+++ src/Components/Cart/CartItems.jsx
@@ -13,7 +13,7 @@ const CartItems = ({
   return (
     <div className={`${styling.containerDiv} rounded-3xl `}>
       <div className="w-[200px]">
-        <img className="w-full rounded-3xl" src={img ? img : "src/assets/defaultburger.png"} alt="" />
+        <img className="w-full rounded-3xl" src={img ? img : "https://as2.ftcdn.net/jpg/03/08/44/33/1000_F_308443353_MEmfG8Jl4VO4FVaXEDY1qoB2Gf3LN26a.jpg"} alt="" />
       </div>
       <div className={`${styling.contentDiv} py-2`}>
         <h1 className={` bungee text-xl ms-5 text-[#ffc400]`}>{name? name : "Your Customized sandwich"}</h1>
